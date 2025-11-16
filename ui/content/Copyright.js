cb3c184fb474a43386f1831dcf57a0afb43fc201
h2roverpete <pete@h2rover.com>
2025-11-15 19:03:10 -0800

    Display bug fix for copyright.

--- ui/content/Copyright.js
+++ ui/content/Copyright.js
@@ -2,27 +2,30 @@ import {useContext} from "react";
 import {SiteContext} from "./Site";
 
 /**
+ * @typedef CopyrightProps
+ *
+ * @property {string} [startYear]
+ */
+
+/**
  * Insert a copyright element.
  *
- * Style: A <div> element with class name "copyright"
+ * Style: A <div> element with class name "Copyright"
  *
- * @param siteData{SiteData}
+ * @param props{CopyrightProps}
  * @returns {JSX.Element}
  * @constructor
  */
 function Copyright(props) {
 
-    const {siteData} = useContext(SiteContext);
+  const {siteData} = useContext(SiteContext);
 
-    return (
-        <>
-          {siteData && (
-              <div className="Copyright">
-                  &copy;{props.startYear?props.startYear:''}-{new Date().getFullYear()} {siteData.SiteName}. All rights reserved.
-              </div>
-          )}
-        </>
-    )
+  return (
+    <div className="Copyright">
+      &copy;{props.startYear ? `${props.startYear}-` : ''}{new Date().getFullYear()} {siteData?.SiteName}. All rights
+      reserved.
+    </div>
+  )
 }
 
 export default Copyright;
