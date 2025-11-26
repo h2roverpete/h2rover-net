70bd93e709d6094deca328a469c7c3508e1bb7c8
h2roverpete <pete@h2rover.com>
2025-11-26 15:25:20 -0800

    Framework 1.1.0 updates.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,9 @@
 import './App.css';
-import './GuestBook.css'
 
-import Head from './framework/ui/content/Head'
-import Copyright from './framework/ui/content/Copyright'
+
 import RestAPI from './framework/api/api';
 import Site from "./framework/ui/content/Site";
-import Page from "./framework/ui/content/Page";
-import PageContent from "framework/ui/content/PageContent";
-import NavBar from "framework/ui/content/NavBar";
-import Breadcrumbs from "framework/ui/content/Breadcrumbs";
-import PageTitle from "framework/ui/content/PageTitle";
-import PageSections from "framework/ui/content/PageSections";
-import GuestBook from "framework/ui/guestbook/GuestBook";
-import Logo from "./Logo";
+import MyPage from "./MyPage";
 
 /**
  * Display navigation bar.
@@ -36,23 +27,14 @@ export default function App() {
 
   const restApi = new RestAPI(
     241,
-    "https://dev.h2rover.net",
+    "https://prod.api.h2rover.net",
     "blahblahblah123"
   );
   return (
-    <Site restApi={restApi} googleId={'G-L4CKYJ846Z'}>
-      <Page>
-        <Head/>
-        <Logo/>
-        <NavBar expand={'sm'} theme={'dark'}/>
-        <PageContent>
-          <Breadcrumbs/>
-          <PageTitle/>
-          <PageSections/>
-          <GuestBook guestBookId={229} pageId={8678}/>
-        </PageContent>
-        <Copyright startYear={'2010'}/>
-      </Page>
-    </Site>
+    <Site
+      restApi={restApi}
+      googleId={'G-L4CKYJ846Z'}
+      pageElement={MyPage}
+    />
   );
 }
---
+++ src/MyPage.js
@@ -0,0 +1,43 @@
+import Head from "framework/ui/content/Head";
+import NavBar from "framework/ui/content/NavBar";
+import PageContent from "framework/ui/content/PageContent";
+import PageTitle from "framework/ui/content/PageTitle";
+import PageSections from "framework/ui/content/PageSections";
+import GuestBook from "framework/ui/guestbook/GuestBook";
+import Copyright from "framework/ui/content/Copyright";
+import Page from "framework/ui/content/Page";
+import React from "react";
+import Logo from "./Logo";
+import Breadcrumbs from "framework/ui/content/Breadcrumbs";
+
+import './GuestBook.css'
+
+/**
+ * @typedef MyPageProps
+ *
+ * @property {number} [pageId]  Specific page ID to display.
+ */
+
+/**
+ * Component for site-specific page contents.
+ *
+ * @param props {MyPageProps}
+ * @returns {JSX.Element}
+ * @constructor
+ */
+export default function MyPage(props) {
+  return (
+    <Page pageId={props.pageId}>
+      <Head/>
+      <Logo/>
+      <NavBar expand={'sm'} theme={'dark'}/>
+      <PageContent>
+        <Breadcrumbs/>
+        <PageTitle/>
+        <PageSections/>
+        <GuestBook guestBookId={229} pageId={8678}/>
+      </PageContent>
+      <Copyright startYear={'2010'}/>
+    </Page>
+  );
+}
