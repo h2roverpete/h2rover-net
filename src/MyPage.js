5a1151fa78179f326d5399e598b320910d3e2b77
h2roverpete <pete@h2rover.com>
2026-03-03 09:12:33 -0800

    Save for framework update.

--- src/MyPage.js
+++ src/MyPage.js
@@ -3,12 +3,10 @@ import NavBar from "framework/ui/content/NavBar";
 import PageContent from "framework/ui/content/PageContent";
 import PageTitle from "framework/ui/content/PageTitle";
 import PageSections from "framework/ui/content/PageSections";
-import Copyright from "framework/ui/content/Copyright";
 import React from "react";
 import Breadcrumbs from "framework/ui/content/Breadcrumbs";
 import Logo from "framework/ui/content/Logo";
 
-import './css/GuestBook.css'
 import logo from "./assets/logo.png"
 import PageNavigation from "framework/ui/content/PageNavigation";
 import PageSwiper from "framework/ui/content/PageSwiper";
@@ -41,7 +39,6 @@ export default function MyPage(props) {
       </PageSwiper>
     </div>
     <PageNavigation/>
-    <Copyright startYear={'2010'}/>
   </>)
     ;
 }
